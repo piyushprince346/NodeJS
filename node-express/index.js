17375a628f5795fedd01be298b8b82aa5447bab5
piyushprince346 <piyushprince346@gmail.com>
2020-07-23 01:37:09 +0530

    Express Serving Static files

--- node-express/index.js
+++ node-express/index.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const http = require('http');
+const morgan = require('morgan');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express() // our application is going to use express node module
+app.use(morgan('dev'));
+
+app.use(express.static(__dirname + '/public'))
 
 app.use((req,res,next) => {
-    console.log(req.headers);
+    // console.log(req.headers); -> if we are using morgan then no need to write this 
     res.statusCode = 200;
     res.setHeader('Content-Type','text/html');
     res.end('<html><body><h1> This is an Express Server </h1></body></html>');
